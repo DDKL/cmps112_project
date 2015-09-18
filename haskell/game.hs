e74bd4c2a94471aebcacde7717a7f5c2f4b005d9
Duke Lee <dukelee92@yahoo.com>
2015-09-18 12:18:21 -0700

    Update game.hs

--- haskell/game.hs
+++ haskell/game.hs
@@ -1,13 +1,13 @@
 import System.Random
 
 play :: (Int, Int) -> (Int, Int) -> IO ()
-play user treasure = do
-    if user == treasure then return ()
+play user prize = do
+    if user == prize then return ()
     else do 
-        putStrLn $ "Distance to your treasure: " ++ (show $ distance user treasure)
+        putStrLn $ "How far are you from your prize? " ++ (show $ dist user prize)
         str <- getLine
         let c = head str
-        play (processInput c user) treasure
+        play (processInput c user) prize
         where
             processInput c p@(x, y) = case c of
                 'n' -> (x, succ y)
@@ -15,7 +15,7 @@ play user treasure = do
                 'e' -> (succ x, y)
                 'w' -> (pred x, y)
                 _   -> p
-            distance (x1, y1) (x2, y2) = sqrt . sum $ map (^2) diffs 
+            dist (x1, y1) (x2, y2) = sqrt . sum $ map (^2) diffs 
                 where diffs = [fromIntegral (x1-x2), fromIntegral (y1-y2)]
 
 main = do
@@ -25,5 +25,5 @@ main = do
     x' <- randomInt
     y' <- randomInt
     play (x, y) (x', y')
-    putStrLn "You found the treasure. Nice!"
+    putStrLn "Prize discovered."
     where randomInt = getStdRandom $ randomR (0, 10)
